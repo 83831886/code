1b2d607ed5f02869aaa51a99544722efb49d941f
trent <83831886@qq.com>
2019-03-14 15:33:49 +0800

    add iptables.sh

---
+++ iptables.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+/sbin/iptables -F
+
+/sbin/iptables -A INPUT -s 127.0.0.1 -d 127.0.0.1 -j ACCEPT
+/sbin/iptables -A INPUT -s 127.0.0.1 -j ACCEPT
+/sbin/iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+/sbin/iptables -A OUTPUT -j ACCEPT
+/sbin/iptables -A INPUT  -p tcp --dport 80  -j ACCEPT
+#允许ping规则
+#/sbin/iptables -A INPUT  -p icmp  -j ACCEPT
+/sbin/iptables -A INPUT -j REJECT
+/sbin/iptables -A FORWARD -j REJECT
+
+/sbin/service iptables save
+/sbin/iptables  -nL
+echo ok
