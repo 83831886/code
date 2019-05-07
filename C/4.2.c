ab62a8fb30ee49fb6f3bbeae5bcfbc1fe3ea7be7
trent <83831886@qq.com>
2019-05-07 16:40:20 +0800

    add C

---
+++ C/4.2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+	char name[40] = "trent";
+	float cash = 334.56;
+
+	printf("The %s family just may be $%.2f dollars richer!\n", name, cash);
+
+	return 0;
+}
