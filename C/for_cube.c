534b12288a9a29c6d2fed3d6ea5d46e23daa7303
trent <83831886@qq.com>
2019-05-10 14:45:51 +0800

    add C

---
+++ C/for_cube.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+int main(void)
+{
+	int num;
+
+	printf("    n   n  cubed\n");
+	for (num = 1;num <= 6;num++)
+		printf("%5d %*d\n", num, strlen("  n  cubed"), num*num*num);
+	
+	return 0;
+}
+
